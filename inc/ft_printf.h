0784c6cc1d20bcac74fe291b2b3024369b3503d7
ailopez- <ailopez-@student.42barcelona.com>
2022-05-19 21:15:03 +0200

    first commit

---
+++ inc/ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ailopez- <ailopez-@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/19 20:56:02 by ailopez-          #+#    #+#             */
+/*   Updated: 2022/05/19 20:56:19 by ailopez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "inc/libft.h"
+# include <stdarg.h>
+# include <string.h>
+# include <stdio.h> 
+# include <stdlib.h>
+# include <unistd.h>
+#endif
